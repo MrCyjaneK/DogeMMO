6cf398c692606d41442c69bfe46829063341fdb0
Czarek Nakamoto <mrcyjanek@gmail.com>
2020-07-29 12:37:29 +0200

    This is not really working, needs some weird hacks to convert mysql to sqlite, but this can be done I think

--- forDevs/db.dev.php
+++ forDevs/db.dev.php
@@ -1,16 +1,20 @@
 <?php
 $token = '690195901'; // For 'historical' reasons
 try {
-$db = NEW PDO("sqlite:".getcwd()."/forDevs/db.dev.db");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
-//$db->query("SET NAMES 'utf8mb4'");
-// Exist in production, cause error in sqlite.
+    if (!file_exists(getcwd()."/forDevs/db.dev.db")) {
+        $db = NEW PDO("sqlite:".getcwd()."/forDevs/db.dev.db");
+        foreach (explode(";\n",file_get_contents(getcwd()."/forDevs/db.scheme.sql")) as $q) {
+            var_dump($q);
+            var_dump($db->query($q));
+        }
+    }
+    $db = NEW PDO("sqlite:".getcwd()."/forDevs/db.dev.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$db->query("SET NAMES 'utf8mb4'");
+    // Exist in production, cause error in sqlite.
 } catch (Exception $e) {
     print_r($e);
-}
-// Seriously, I don't know why is this function here.. but it's in db.production.php so DON'T remove it.
-function n($n) {
-  return number_format($n, 8, '.', '');
+    die('fatal error in db.dev.php');
 }
 // TEST:
 //$q = $db->prepare('SELECT * FROM `users`');
